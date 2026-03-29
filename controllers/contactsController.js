9328b1aabce1468a35b8c20573fc3ea49eedaf69
Craig Briese <craig.briese@gmail.com>
2026-03-29 02:26:09 -0500

    Added PostgreSQL database connectivity

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,12 +1,32 @@
 // controllers/contactsController.js
-exports.getAllContacts = (req, res) => {
+
+//
+// I guess try to make a connection to the database?
+//
+
+const pool = require('../db.js');
+
+exports.getAllContacts = async (req, res) => {
 	console.log("Request for all contacts received");
-	res.send('{}');
+	const result = await pool.query('SELECT * FROM contacts');
+	res.status(200).json(result.rows);
 };
 
-exports.getContactById = (req, res) => {
-	console.log("Request for a single contact received");
-	res.send('{}');
+exports.getContactById = async (req, res) => {
+	console.log("Request for a single contact with id " + req.params.id + " received");
+	try {
+		const result = await pool.query("SELECT * FROM contacts WHERE id = $1", [req.params.id]);
+
+		if (result.rows.length === 0) {
+			return res.status(404).send();
+		}
+
+		res.status(200).json(result.rows[0]);
+
+	} catch (err) {
+		console.log(err);
+		res.status(500).send();
+	}
 };
 
 exports.createContact = (req, res) => {
--- run_dev.sh
+++ run_dev.sh
@@ -3,42 +3,28 @@
 # Bring in the environment
 source .env.dev
 
-if [ -z "${CERTS_DIR}" ]
-then
-	echo -e "\n  Error: environment variable CERTS_DIR not defined\n\n" 1>&2
-	exit 2
-fi
+required_variables=("CERTS_DIR" "IMAGE_NAME" "CONTAINER_NAME" "SERVER_NAME" "SSL_PORT"
+	"DB_HOST" "DB_NAME")
 
-if [ -z "${IMAGE_NAME}" ]
-then
-	echo -e "\n  Error: environment variable IMAGE_NAME not defined\n\n" 1>&2
-	exit 2
-fi
 
-if [ -z "${CONTAINER_NAME}" ]
-then
-	echo -e "\n  Error: environment variable CONTAINER_NAME not defined\n\n" 1>&2
-	exit 2
-fi
-
-if [ -z "${SERVER_NAME}" ]
-then
-	echo -e "\n  Error: environment variable SERVER_NAME is not defined\n\n" 1>&2
-	exit 2
-fi
-
-if [ -z "${SSL_PORT}" ]
-then
-	echo -e "\n  Error: environment variable SSL_PORT is not defined\n\n" 1>&2
-	exit 2
-fi
-
+for var in ${required_variables[@]}; do
+	if [ -z "${!var}" ]
+	then
+		echo -e "\n  Error: environment variable ${var} not defined\n" 1>&2
+		exit 2
+	fi
+done
 
 podman run -d \
 	--replace \
 	--name ${CONTAINER_NAME} \
+	--network=host \
 	-p ${SSL_PORT}:${SSL_PORT} \
 	-e SERVER_NAME=${SERVER_NAME} \
+	-e DB_HOST=${DB_HOST} \
+	-e DB_NAME=${DB_NAME} \
+	--secret contacts_db_username,type=env,target=DB_USER \
+	--secret contacts_db_password,type=env,target=DB_PASSWORD \
 	-v ${CERTS_DIR}:/etc/ssl:ro,Z \
 	${IMAGE_NAME}
 
